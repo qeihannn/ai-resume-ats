5812f1513d35c76efaa61a9aeaa0cbfba02d4a40
qeihannn <binggoqeihan@gmail.com>
2025-12-21 16:09:55 +0700

    style: adjust formatting and indentation in Home component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -18,15 +18,15 @@ export default function Home() {
           <h1>Track Your Application & Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedback</h2>
         </div>
-      </section> 
 
-      {resumes.length > 0 &&(
-      <div className="resumes-section">
-        {resumes.map( (resume) => (
-          <ResumeCard key={resume.id} resume={resume} />
-        ))}
-      </div>
-      )}
+        {resumes.length > 0 &&(
+          <div className="resumes-section">
+          {resumes.map( (resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+        )}
+      </section> 
 
 
   </main>
