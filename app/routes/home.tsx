f3aa09228ac2b33e6d34f9c50173a6d2a12d2793
qeihannn <binggoqeihan@gmail.com>
2025-12-22 01:54:22 +0700

    feat: implement authentication check in Home component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,10 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import { resumes } from "../../constant";
 import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +15,13 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect( () => {
+        if (!auth.isAuthenticated) navigate('/auth?next=/');
+    },  [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
